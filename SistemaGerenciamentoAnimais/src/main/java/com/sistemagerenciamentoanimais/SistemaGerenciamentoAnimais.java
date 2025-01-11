5a7eb551be41e2b6e1b753c147dddf345428a09a
João Marcelo <159437965+jm-236@users.noreply.github.com>
2025-01-11 08:56:51 -0300

    Criação inicial do projeto.

---
+++ SistemaGerenciamentoAnimais/src/main/java/com/sistemagerenciamentoanimais/SistemaGerenciamentoAnimais.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sistemagerenciamentoanimais;
+
+/**
+ *
+ * @author Cynthia
+ */
+public class SistemaGerenciamentoAnimais {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
